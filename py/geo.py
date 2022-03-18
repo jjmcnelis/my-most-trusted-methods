23708b771edfa72b37254470cd844f98927cd855
Jack McNelis <jjmcnelis@outlook.com>
2022-03-18 14:39:27 -0400

    Create geo.py

---
+++ py/geo.py
@@ -0,0 +1,16 @@
+from math import radians, cos, sin, asin, sqrt
+
+def haversine_distance(lon1: float, lat1: float, lon2: float, lat2: float, a: float=6378137.):
+    """https://en.wikipedia.org/wiki/Haversine_formula
+    :param lon1: Longitude of the first xy coordinate pair
+    :param lat1: Latitude of the first xy coordinate pair
+    :param lon2: Longitude of the second xy coordinate pair
+    :param lat2: Latitude of the second xy coordinate pair
+    :param a: Equatorial radius of reference ellipse (meters)
+    :returns: Great circle distance between input lon, lat pairs (meters)
+    """
+    lon1, lon2 = radians(lon1), radians(lon2)
+    lat1, lat2 = radians(lat1), radians(lat2)
+    haversine = sin((lat2-lat1)/2)**2 + cos(lat1) * cos(lat2) * sin((lon2-lon1)/2)**2
+    ihaversine = 2 * asin(sqrt(haversine))
+    return(ihaversine * a)
