d4b0a5a6eff23b7edeef08ff79b7c4e35b2bb00e
Jack McNelis <jjmcnelis@outlook.com>
2022-03-18 15:08:58 -0400

    Update cmn.py

--- py/cmn.py
+++ py/cmn.py
@@ -1,19 +1,39 @@
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 import requests
-import os.path
+import hashlib
+import json
+import sys
+import os
+
 
 def modularize(module_file: str, module_name: str):
     initf = os.path.join(module_file, module_name, '__init__.py')
     if not os.path.exists(initf):
         open(initf, 'wb').close()
-  
-def import_stats():
+
+
+def list_imports():
+    _imports = []
     for k,v in globals().items():
         if 'module' in str(type(v)):
             if hasattr(v, "__path__"):
-                print(f"  {v.__name__}  \t{v.__version__ }\t({v.__path__[0]})")
+                _imports.append((v.__name__,v.__version__,v.__path__[0]))
+    return _imports
 
-def download_file(url: str, target: str, force: bool=False):
+
+def walk_dir(path: str, extn: str=None, skip: list=None) -> list:
+    _files = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            fext = os.path.splitext(f)[1]
+            if extn is None or extn in fext:
+                if skip is None or fext not in skip:
+                    _files.append(os.path.join(root, f))
+    return _files
+
+
+def download(url: str, target: str, force: bool=False) -> str:
     if not os.path.isfile(target) or force is True:
         with requests.get(url) as response, open(target, 'wb') as file:
             if not response.status_code // 100 == 2: 
@@ -23,6 +43,27 @@ def download_file(url: str, target: str, force: bool=False):
                     file.write(chunk)
     return target
 
-def download_pool(jobs: list, max_workers: int=12) -> list:
+
+def download_batch(urls: list, max_workers: int=12) -> list:
     with ThreadPoolExecutor(max_workers=max_workers) as pool:
-        return list(tqdm(pool.map(lambda x: download_file(*x), jobs), total=len(jobs)))
+        return list(tqdm(pool.map(lambda x: download(*x), urls), total=len(urls)))
+
+
+def file_hash(file: str, blocksize=4096):
+    """Return hex digest for the input `file` in chunks of `blocksize`"""
+    sha = hashlib.sha256()
+    with open(file, 'rb') as fp:
+        while 1:
+            data = fp.read(blocksize)
+            if data:
+                sha.update(data)
+            else:
+                break
+    return sha.hexdigest()
+
+
+def file_stat(file: str, mult: float=0.001, tfmt: str="%Y-%m-%d %H:%M:%S"):
+    stat = os.stat(file)
+    return {"name": os.path.basename(file), 
+            "size": stat.st_size * mult, 
+            "date": datetime.fromtimestamp(stat.st_mtime).strftime(tfmt), }
